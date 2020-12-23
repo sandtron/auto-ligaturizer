e1fc933358192d3c0ad50bba0112d1ed02c7c952
mrsandtron <mrsandtron@protonmail.com>
2020-12-22 21:44:23 -0500

    added some 'logging'

--- app/Ligaturizer.rb
+++ app/Ligaturizer.rb
@@ -8,7 +8,7 @@ class Ligaturizer
 
   def ligaturize(binary)
     uid = SecureRandom.uuid.to_s
-    tmp_dir = `echo $(pwd)/tmp/#{uid}/`.strip
+    tmp_dir = `echo $(pwd)/tmp/#{uid}`.strip
 
     FileUtils.mkdir_p(tmp_dir)
 
@@ -16,8 +16,10 @@ class Ligaturizer
 
     File.open(temp_in_file_name, 'wb') { |io| io.write(binary) }
 
-    docker_result = `docker run --rm -v #{temp_in_file_name}:/input -v #{tmp_dir}:/output --user $(id -u) rfvgyhn/ligaturizer`
-    # puts docker_result
+    docker_cmd = `echo docker run --rm -v #{temp_in_file_name}:/input -v #{tmp_dir}:/output --user $(id -u) rfvgyhn/ligaturizer`
+    puts docker_cmd.to_s
+    docker_result = `#{docker_cmd}`
+    puts docker_result
     temp_out_file_name = "#{tmp_dir}/#{extract_result_file_name(docker_result)}"
 
     result = IO.binread(temp_out_file_name)
