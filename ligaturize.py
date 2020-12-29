b388c80fc69c66915780d4d686119ec1077413ec
mrsandtron <mrsandtron@protonmail.com>
2020-12-29 16:44:20 -0500

    added python2 unicdoe fix

--- ligaturize.py
+++ ligaturize.py
@@ -3,7 +3,37 @@ import sys
 import fontforge
 import json
 
+
+def _decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+
+def _decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, unicode):
+            key = key.encode('utf-8')
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        elif isinstance(value, list):
+            value = _decode_list(value)
+        elif isinstance(value, dict):
+            value = _decode_dict(value)
+        rv[key] = value
+    return rv
+
+
 ligature_source_dir = os.path.join(sys.path[0], "ligature-sources/firacode")
+
 in_file = sys.argv[1]
 
 try:
@@ -20,7 +50,7 @@ config = {
     }}
 
 with open(os.path.join(ligature_source_dir, 'mapping.json'), 'r') as cfg:
-    jsonCfg = json.load(cfg)
+    jsonCfg = json.load(cfg, object_hook=_decode_dict)
     config["add_ligatures"] = jsonCfg["add_ligatures"]
     config["ligature_source_ttf"] = os.path.join(
         ligature_source_dir, jsonCfg["source_ligature_filename"])
